4a19db44de1d3f4df595d2a44961bfed67d7bfd0
uridanan <uridanan@users.noreply.github.com>
2023-09-05 22:50:46 +0300

    Add authors + quotes
    Add authors
    Add a few more quotes

--- quotes.js
+++ quotes.js
@@ -1,9 +1,11 @@
 //quote array
 var quotes = [
-    "A good leader does not give all the right answers, he asks all the right questions",
-    "Don't practice until you get it right, practice until you can't get it wrong",
-    "Natural Stupidity is still more dangerous than Artificial Intelligence",
-    "It takes as much energy to wish as it does to plan"
+    {quote: "A good leader does not give all the right answers, he asks all the right questions", author: "Tim Brown"},
+    {quote: "Don't practice until you get it right, practice until you can't get it wrong", author: "John Flanagan"},
+    {quote: "Natural Stupidity is still more dangerous than Artificial Intelligence", author: "Albert Einstein ?"},
+    {quote: "It takes as much energy to wish as it does to plan", author: "Eleanor Roosevelt"},
+    {quote: "Nobody realizes that some people expend tremendous energy merely to be normal.", author: "Albert Camus"},
+    {quote: "Those who are too smart to engage in politics are punished by being governed by those who are dumber.", author: "Plato"}
     ];
 
 
@@ -24,7 +26,9 @@ function getRandomQuote(){
 }
 
 function updateContent(){
-    document.getElementById('quote').innerHTML = getRandomQuote();
+    q = getRandomQuote();
+    document.getElementById('quote').innerHTML = q.quote;
+    document.getElementById('author').innerHTML = q.author;
 }
 
 updateContent();
